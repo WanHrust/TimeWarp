6044d2e9052c5a53b3eddae5d1e7d188305d6782
WanHrust <rustam.alashrafov@gmail.com>
2018-03-01 00:52:28 +0100

    Initial Commit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    public Transform target;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(target.position - gameObject.transform.position);
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        if(collider.gameObject.CompareTag("InfluenceSphere")) {
+            Debug.Log("Sphere Colliding");
+        }
+    }
+}
