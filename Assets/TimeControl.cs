eec03dcc188ad5961fd87e5202048deace446e91
WanHrust <rustam.alashrafov@gmail.com>
2018-04-03 23:35:30 +0200

    Added influence ripple shader

--- Assets/TimeControl.cs
+++ Assets/TimeControl.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class TimeControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    Rigidbody rb;
+    private float _timeScale = 1.0f;
+    public float timeScale {
+        get { return _timeScale; }
+        set {
+
+            rb.mass *= timeScale;
+            rb.velocity /= timeScale;
+            rb.angularVelocity /= timeScale;
+
+            _timeScale = Mathf.Abs(value);
+
+            rb.mass /= timeScale;
+            rb.velocity *= timeScale;
+            rb.angularVelocity *= timeScale;
+        }
+    }
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+    /*
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.CompareTag("InfluenceField")) {
+            timeScale = 0.01f;
+        }
+    }
+    void OnTriggerExit(Collider collider) {
+        if (collider.gameObject.CompareTag("InfluenceField")) {
+            timeScale = 1.0f;
+        }
+    }
+    */
+    // Update is called once per frame
+    void Update () {
+       
 	}
 }
